e300eeaf6cfe42ed6fbf2dd0e518482f3c33e056
Arkan Jbali <arkan.1997@gmail.com>
2019-01-09 21:57:10 +0200

    Adding Queries

--- src/main/java/com/log/loggitor/LoggitorApplication.java
+++ src/main/java/com/log/loggitor/LoggitorApplication.java
@@ -3,7 +3,7 @@ package com.log.loggitor;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,38 +50,38 @@ public class LoggitorApplication {
 	@Bean
 	CommandLineRunner runner(){
         return args -> {
-    		//split the path to take the file name
-			String fname="C:\\Users\\arkan\\Downloads\\log\\ARServer.log";
-			String strFileName=fname.substring(fname.lastIndexOf("\\")+1);
-			//LogReader that read the file
-			LogReader lr=new LogReader();
-			lr.ReadLog(fname);
-			//get the errors list for doing the process
-			ArrayList<String> errors=lr.getErrorList();
-			//processing the errors list and storing them with counting
-			LogProcess process=new LogProcess();
-			//Log p=new Log();
-			process.analyze(errors);
-			//String severity=process.ErrorsPerAppSeverity();
-			
-			ArrayList<Log> plog=process.getErrorAll();
-			FileRep.deleteAll();
-			DefIRep.deleteAll();
-			AppRep.deleteAll();
-        	SolRep.deleteAll();
-        	DefRep.deleteAll();
-        	
-			Date date=Calendar.getInstance().getTime();
-			FileRep.save(new LogFile(strFileName, date));
-			for(Log b:plog) {
-				AppRep.save(new App(b.getErrorName(),b.getErrorType()));
-				DefRep.save(new Defects(b.getAppSeverity(), b.getErrorNo(),1));
-			}
+//    		//split the path to take the file name
+//			String fname="C:\\Users\\arkan\\Downloads\\log\\ARServer.log";
+//			String strFileName=fname.substring(fname.lastIndexOf("\\")+1);
+//			//LogReader that read the file
+//			LogReader lr=new LogReader();
+//			lr.ReadLog(fname);
+//			//get the errors list for doing the process
+//			ArrayList<String> errors=lr.getErrorList();
+//			//processing the errors list and storing them with counting
+//			LogProcess process=new LogProcess();
+//			//Log p=new Log();
+//			process.analyze(errors);
+//			//String severity=process.ErrorsPerAppSeverity();
+//			
+//			ArrayList<Log> plog=process.getErrorAll();
+//			FileRep.deleteAll();
+//			DefIRep.deleteAll();
+//			AppRep.deleteAll();
+//        	SolRep.deleteAll();
+//        	DefRep.deleteAll();
+//        	
+//			Date date=Calendar.getInstance().getTime();
+//			FileRep.save(new LogFile(strFileName, date));
+//			for(Log b:plog) {
+//				AppRep.save(new App(b.getErrorName(),b.getErrorType()));
+//				DefRep.save(new Defects(b.getAppSeverity(), b.getErrorNo(),1));
+//			}
 
          //Save demo data to database
         //AppRep.save(new App("CM","Core"));
-        	SolRep.save(new Solutions("Solution1","Description"));
-        	DefIRep.save(new DefectInstance(1, 1, 1));
+//        	SolRep.save(new Solutions("Solution1","Description"));
+//        	DefIRep.save(new DefectInstance(1, 1, 1));
         	
         	//App p=new App(plog.get(0).getErrorName(),plog.get(0).getErrorType());
         	//DefIRep.save(new DefectInstance(1, 1, 1,p));
@@ -90,6 +90,9 @@ public class LoggitorApplication {
 //        	List<App> apps=AppRep.findByAppName("BL");
 //        	for(App bb:apps)
 //        		System.out.println(bb.getAppName()+" "+bb.getAppType());
+        	for(App app:AppRep.DefectsByApp()) {
+        		logger.info(app.getAppName());
+        	}
         };
       } 
 
